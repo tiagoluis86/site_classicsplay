d8e1f6fbdb7e7227a970cd986ed46ea7413a6059
tiagoluis86 <70555750+tiagoluis86@users.noreply.github.com>
2021-12-29 08:43:03 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# classicsplay
+Exercídio de HTML, CSS e Javascript onde tentei recriar a interface da Netflix como se fosse um serviço de games
---
+++ js/main.js
@@ -0,0 +1,10 @@
+function redirecionar1() {
+    window.open("https://www.nintendo.com");
+}
+
+function redirecionar2() {
+        window.open("https://www.gamedatabase.com");
+}
+
+
+
